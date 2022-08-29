e0e4b0f487d734d53fd419dc7c3b6c7092e9702d
LiliyaRafikova <109327816+LiliyaRafikova@users.noreply.github.com>
2022-08-29 14:32:32 +0500

    Update Program.cs

--- 0052/Program.cs
+++ 0052/Program.cs
@@ -17,25 +17,6 @@ for (int i = 0; i < row; i++)
      Console.WriteLine();
 }
 
-﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-Console.Write("Введите число строк: ");
-int row = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число столбов: ");
-int col = Convert.ToInt32(Console.ReadLine());
-
-int [,] Array = new int[row, col];//основной массив
-
-for (int i = 0; i < row; i++)
-{
-    for (int j = 0; j < col; j++)
-    {
-        Array[i, j] = new Random().Next(100);
-        Console.Write(Array[i, j]+"\t");
-    }
-     Console.WriteLine();
-}
-
 double[] massiv = new double[col];// массив для вывода суммы элементов столбца
 for (int j = 0; j < col; j++)
 	{
